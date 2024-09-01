a79c8ce41efbc7f15443f6d6ad9d896a4fff79b2
Abel Albor <abelalbor004@gmail.com>
2024-09-01 14:47:03 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import viteLogo from './assets/vite-logo.png'
 import muiLogo from './assets/mui-logo.png'
 import prestamosImg from './assets/prestamos.png'
 
-import hojaDeVida from './assets/HOJA_DE_VIDA_AA.pdf'
+import hojaDeVida from './assets/HOJA DE VIDA AA.pdf'
 
 import { RiArrowDownWideLine } from "react-icons/ri";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
